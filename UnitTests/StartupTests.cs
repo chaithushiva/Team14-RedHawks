6e4023c677d5670b41946679829331e1c7da38cf
chaithushiva <sgoli@seatlleu.edu>
2022-11-08 07:49:34 -0800

    Created Unit Tests for Startup <StartupTests.cs>

--- UnitTests/StartupTests.cs
+++ UnitTests/StartupTests.cs
@@ -1,25 +1,38 @@
-using Microsoft.Extensions.Configuration;
-using Microsoft.AspNetCore.Hosting;
-
-using NUnit.Framework;
-
-namespace UnitTests.Pages.Startup
+namespace UnitTests
 {
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+
+    using NUnit.Framework;
+
+
+    /// <summary>
+    /// Set of initial tests.
+    /// </summary>
     public class StartupTests
     {
         #region TestSetup
 
+        /// <summary>
+        /// Initialize the test.
+        /// </summary>
         [SetUp]
         public void TestInitialize()
         {
         }
 
+        /// <summary>
+        /// Start up the Contoso crafts website.
+        /// </summary>
         public class Startup : ContosoCrafts.WebSite.Startup
         {
             public Startup(IConfiguration config) : base(config) { }
         }
         #endregion TestSetup
 
+        /// <summary>
+        /// Configure services after Start up.
+        /// </summary>
         #region ConfigureServices
         [Test]
         public void Startup_ConfigureServices_Valid_Defaut_Should_Pass()
@@ -28,7 +41,6 @@ namespace UnitTests.Pages.Startup
             Assert.IsNotNull(webHost);
         }
         #endregion ConfigureServices
-
         #region Configure
         [Test]
         public void Startup_Configure_Valid_Defaut_Should_Pass()
