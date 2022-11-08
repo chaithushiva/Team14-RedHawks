2da6eeb928a6c01be6ea52cd8248ca4cf1c07278
Pranindhar3123 <pranindhar111@gmail.com>
2022-11-08 15:15:22 -0800

    Added comments for product model <ProductModel.cs>

--- src/Models/ProductModel.cs
+++ src/Models/ProductModel.cs
@@ -8,46 +8,46 @@ namespace ContosoCrafts.WebSite.Models
     public class ProductModel
     {
         /// <summary>
-        /// Define attributes of Article Model. 
+        /// Define attributes of Product Model. 
         /// </summary>
-        // Properties: Unique Identifier, Author(s), Publish Date, URL, Image, Title, Tags, Article Text/Body.
+        // Properties: Unique Identifier, Author(s), Publish Date, URL, Image, Title, Tags, Product Text/Body.
 
-        // Unique identifier for the article
+        // Unique identifier for the product
         public string Id { get; set; }
 
         // Author Name
         public string Author { get; set; }
 
-        // A link to the image file to display in article preview
+        // A link to the image file to display in product preview
         [JsonPropertyName("img")]
         public string Image { get; set; }
 
-        // Web location for the article
+        // Web location for the product
         public string Url { get; set; }
 
-        // Article title
+        // Product title
         [StringLength(maximumLength: 128, MinimumLength = 1, ErrorMessage = "The SchoolName should have a length of more than {2} and up to {1} characters")]
         public string SchoolName { get; set; }
 
-        // Web location for the article
+        // Web location for the product
         public string SchoolAddress { get; set; }
 
-        // Web location for the article
+        // Web location for the product
         public string SchoolContactInfo { get; set; }
 
-        // Web location for the article
+        // Web location for the product
         public string SchoolEmail { get; set; }
 
-        // ratings for the article (remnant from Contoso Crafts site)
+        // ratings for the product (remnant from Contoso Crafts site)
         public int[] Ratings { get; set; }
 
-        // An enum to decription the article category (tag)
+        // An enum to decription the product category (tag)
         public ProductTypeEnum ProductType { get; set; } = ProductTypeEnum.Undefined;
 
-        // Store the Comments entered by the users on this article.
+        // Store the Comments entered by the users on this product.
         public List<CommentModel> CommentList { get; set; } = new List<CommentModel>();
 
-        // To string method to display article as a text stirng
+        // To string method to display product as a text stirng
         public override string ToString() => JsonSerializer.Serialize<ProductModel>(this);
     }
 }
