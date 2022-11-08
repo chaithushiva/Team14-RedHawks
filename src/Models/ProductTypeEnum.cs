90e3de90befd2ff57465dd967a982b5b4fad8fe7
chaithushiva <sgoli@seatlleu.edu>
2022-11-08 03:28:55 -0800

    Created product type for products <ProductTypeEnum.cs>

--- src/Models/ProductTypeEnum.cs
+++ src/Models/ProductTypeEnum.cs
@@ -1,30 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace ContosoCrafts.WebSite.Models
+﻿namespace ContosoCrafts.WebSite.Models
 {
+    /// <summary>
+    /// Define an enum for type Article. 
+    /// </summary>
     public enum ProductTypeEnum
     {
         Undefined = 0,
-        Amature = 1,
-        Antique = 5,
-        Collectable = 130,
-        Commercial = 55,
+        Laptops = 1,
+        Projectors = 2,
+        Desks = 3,
+        SmartBoards = 4,
+        NoteBooks = 6,
     }
-
+    /// <summary>
+    /// Define Article Type enum extensions. 
+    /// </summary>
     public static class ProductTypeEnumExtensions
     {
+        /// <summary>
+        /// Display name of the Article type enum. 
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
         public static string DisplayName(this ProductTypeEnum data)
         {
             return data switch
             {
-                ProductTypeEnum.Amature => "Hand Made Items",
-                ProductTypeEnum.Antique => "Antiques",
-                ProductTypeEnum.Collectable => "Collectables",
-                ProductTypeEnum.Commercial => "Commercial goods",
- 
+                ProductTypeEnum.Laptops => "Electronic Items",
+                ProductTypeEnum.Projectors => "Electronic Items",
+                ProductTypeEnum.Desks => "Stationery Items",
+                ProductTypeEnum.SmartBoards => "Electronic Items",
+                ProductTypeEnum.NoteBooks => "Stationery Items",
+
                 // Default, Unknown
                 _ => "",
             };
