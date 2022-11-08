6bcde0835765a14aa34b3bc0e5d8fa64ac8fb385
chaithushiva <sgoli@seatlleu.edu>
2022-11-08 03:28:21 -0800

    Created Models for products <ProductModel.cs>

--- src/Models/ProductModel.cs
+++ src/Models/ProductModel.cs
@@ -1,39 +1,44 @@
-using System.Text.Json;
-using System.Text.Json.Serialization;
-using System.ComponentModel.DataAnnotations;
-using System.Collections.Generic;
-
 namespace ContosoCrafts.WebSite.Models
 {
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Text.Json;
+    using System.Text.Json.Serialization;
+
     public class ProductModel
     {
-        public string Id { get; set; }
-        public string Maker { get; set; }
+        /// <summary>
+        /// Define attributes of Article Model. 
+        /// </summary>
+        // Properties: Unique Identifier, Author(s), Publish Date, URL, Image, Title, Tags, Article Text/Body.
 
+        // Unique identifier for the article
+        public string Id { get; set; }
+
+        // Author Name
+        public string Author { get; set; }
+
+        // A link to the image file to display in article preview
         [JsonPropertyName("img")]
         public string Image { get; set; }
 
+        // Web location for the article
         public string Url { get; set; }
-        
-        [StringLength (maximumLength: 33, MinimumLength = 1, ErrorMessage = "The Title should have a length of more than {2} and less than {1}")]
-        public string Title { get; set; }
 
-        public string Description { get; set; }
+        // Article title
+        [StringLength(maximumLength: 128, MinimumLength = 1, ErrorMessage = "The SchoolName should have a length of more than {2} and up to {1} characters")]
+        public string SchoolName { get; set; }
 
+        // ratings for the article (remnant from Contoso Crafts site)
         public int[] Ratings { get; set; }
 
+        // An enum to decription the article category (tag)
         public ProductTypeEnum ProductType { get; set; } = ProductTypeEnum.Undefined;
 
-        public string Quantity { get; set; }
-
-        [Range (-1, 100, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
-        public int Price { get; set; }
-
-        // Store the Comments entered by the users on this product
+        // Store the Comments entered by the users on this article.
         public List<CommentModel> CommentList { get; set; } = new List<CommentModel>();
 
+        // To string method to display article as a text stirng
         public override string ToString() => JsonSerializer.Serialize<ProductModel>(this);
-
-
     }
 }
