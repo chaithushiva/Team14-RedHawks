18dded5548b73e48e57150293ff735d62fdc24f3
chaithushiva <sgoli@seatlleu.edu>
2022-11-08 03:38:45 -0800

    Created .cs file for Create page <Create.cshtml.cs>

---
+++ src/Pages/Shared/Product/Create.cshtml.cs
@@ -0,0 +1,40 @@
+namespace ContosoCrafts.WebSite.Pages.Product
+{
+    using ContosoCrafts.WebSite.Models;
+    using ContosoCrafts.WebSite.Services;
+
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+
+    /// <summary>
+    /// Create Page
+    /// </summary>
+    public class CreateModel : PageModel
+    {
+        // Data middle tier
+        public JsonFileProductService ProductService { get; }
+
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        /// <param name="productService">The service responsible for interacting with the data store.</param>
+        public CreateModel(JsonFileProductService productService)
+        {
+            ProductService = productService;
+        }
+
+        // The data to show
+        public ProductModel Product;
+
+        /// <summary>
+        /// REST Get request
+        /// </summary>
+        /// <returns>Redirect the web page to the Update page populated with the data so the user can fill in the fields.</returns>
+        public IActionResult OnGet()
+        {
+            this.Product = ProductService.CreateData();
+
+            return RedirectToPage("./Update", new { this.Product.Id });
+        }
+    }
+}
