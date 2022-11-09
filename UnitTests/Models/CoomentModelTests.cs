aa20c8485e79cfa64a5dc1ac492f828e6380a752
Pranindhar3123 <pranindhar111@gmail.com>
2022-11-08 21:38:38 -0800

    Added Unit Tests for Comment <CommentModelTests.cs>

---
+++ UnitTests/Models/CoomentModelTests.cs
@@ -0,0 +1,27 @@
+﻿namespace UnitTests.Models
+{
+    using ContosoCrafts.WebSite.Models;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Test class for CommentModelTests
+    /// </summary>
+    public class CommentModelTests
+    {
+
+        [Test]
+        public void CommentModel_Valid_Null_ID_Should_Return_True()
+        {
+            // Arrange
+            CommentModel result = new CommentModel();
+
+            // Act
+            result.Id = "a1feba95-561f-46c1-8e88-db0ef2f8a2a4";
+            result.Comment = "Test comment";
+
+            // Assert
+            Assert.AreEqual("a1feba95-561f-46c1-8e88-db0ef2f8a2a4", result.Id);
+            Assert.AreEqual("Test comment", result.Comment.ToString());
+        }
+    }
+}
