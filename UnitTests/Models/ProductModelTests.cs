7a01f1878531c78786d51670186bdfd003b89ecd
Pranindhar3123 <pranindhar111@gmail.com>
2022-11-08 18:19:45 -0800

    Created Unit tests for product model <ProductModelTests.cs>

---
+++ UnitTests/Models/ProductModelTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace UnitTests.Models
+{
+    /// <summary>
+    /// Test class for ArticleModel
+    /// </summary>
+    public class ProductModelTests
+    {
+        /// <summary>
+        /// Method to test valid input of an article
+        /// </summary>
+        [Test]
+        public void ToString_Valid_Null_ID_Should_Return_True()
+        {
+            // Arrange
+            var data = TestHelper.ProductService.GetAllData().First(x => x.Id == "jenlooper-cactus");
+
+            // Act
+            var result = TestHelper.ProductService.GetAllData().First();
+
+            // Assert
+            Assert.AreEqual(data.ToString(), result.ToString());
+
+        }
+    }
+}
