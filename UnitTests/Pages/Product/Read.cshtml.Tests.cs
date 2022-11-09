fae776b82720a6ab8fc5b6d5842c0bd053848c4f
Pranindhar3123 <pranindhar111@gmail.com>
2022-11-08 23:43:25 -0800

    Created Controller for Products <ProductsControllerTests.cs>

--- UnitTests/Pages/Product/Read.cshtml.Tests.cs
+++ UnitTests/Pages/Product/Read.cshtml.Tests.cs
@@ -1,118 +1,155 @@
-﻿namespace UnitTests.Pages.Product
+﻿using ContosoCrafts.WebSite.Pages;
+using ContosoCrafts.WebSite.Pages.Product;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace UnitTests.Pages.Product
 {
-    using System.Linq;
 
-    using ContosoCrafts.WebSite.Pages.Product;
-    using ContosoCrafts.WebSite.Services;
 
-    using Microsoft.AspNetCore.Hosting;
-    using Microsoft.AspNetCore.Http;
-    using Microsoft.AspNetCore.Mvc;
-    using Microsoft.AspNetCore.Mvc.ModelBinding;
-    using Microsoft.AspNetCore.Mvc.RazorPages;
-    using Microsoft.AspNetCore.Mvc.Routing;
-    using Microsoft.AspNetCore.Mvc.ViewFeatures;
-    using Microsoft.AspNetCore.Routing;
-    using Microsoft.Extensions.Logging;
-
-    using Moq;
-
-    using NUnit.Framework;
 
     /// <summary>
-    /// This class holds the tests for the Read.cshtml.Tests.cs.
+    /// Class containing unit test cases for Read page
     /// </summary>
     public class ReadTests
     {
+
+
+
+        // Creating an instance
         #region TestSetup
-        // A factory for creating IUrlHelper instances.
-        public static IUrlHelperFactory urlHelperFactory;
 
-        // Represents an implementation of the HTTP Context class.
-        public static DefaultHttpContext httpContextDefault;
 
-        // Provides information about the web hosting environment an application is running in.
-        public static IWebHostEnvironment webHostEnvironment;
 
-        // Represents the state of an attempt to bind a posted form to an action method, which includes validation information.
-        public static ModelStateDictionary modelState;
-
-        // Context object for execution of action which has been selected as part of an HTTP request.
-        public static ActionContext actionContext;
-
-        // Represents an empty model metadata provider.
-        public static EmptyModelMetadataProvider modelMetadataProvider;
-
-        // Represents a container that is used to pass data between a controller and a view.
-        public static ViewDataDictionary viewData;
-
-        // Represents a set of data that persists only from one request to the next.
-        public static TempDataDictionary tempData;
-
-        // The SharePoint page context object.
-        public static PageContext pageContext;
-
-        // Data field for ReadModel
         public static ReadModel pageModel;
 
 
+
         /// <summary>
-        /// Setup test prior to execution.
+        ///  Initializing Tests
         /// </summary>
         [SetUp]
         public void TestInitialize()
         {
-            httpContextDefault = new DefaultHttpContext()
+            pageModel = new ReadModel(TestHelper.ProductService)
             {
-                //RequestServices = serviceProviderMock.Object,
-            };
 
-            modelState = new ModelStateDictionary();
 
-            actionContext = new ActionContext(httpContextDefault, httpContextDefault.GetRouteData(), new PageActionDescriptor(), modelState);
 
-            modelMetadataProvider = new EmptyModelMetadataProvider();
-            viewData = new ViewDataDictionary(modelMetadataProvider, modelState);
-            tempData = new TempDataDictionary(httpContextDefault, Mock.Of<ITempDataProvider>());
-
-            pageContext = new PageContext(actionContext)
-            {
-                ViewData = viewData,
-            };
-
-            var mockWebHostEnvironment = new Mock<IWebHostEnvironment>();
-            _ = mockWebHostEnvironment.Setup(m => m.EnvironmentName).Returns("Hosting:UnitTestEnvironment");
-            _ = mockWebHostEnvironment.Setup(m => m.WebRootPath).Returns("../../../../src/bin/Debug/net6.0/wwwroot");
-            _ = mockWebHostEnvironment.Setup(m => m.ContentRootPath).Returns("./data/");
-
-            var MockLoggerDirect = Mock.Of<ILogger<IndexModel>>();
-            JsonFileProductService productService;
-
-            productService = new JsonFileProductService(mockWebHostEnvironment.Object);
-
-            pageModel = new ReadModel(productService)
-            {
             };
         }
+
+
 
         #endregion TestSetup
 
+
+
+        #region GetallData
+
+
+
         /// <summary>
-        /// Test valid result from OnGet method.
+        ///  Testing GetallData for invalid product null should return false
         /// </summary>
-        #region OnGet
+        [Test]
+        public void GetAllData_InValid_Product_Null_Should_Return_False()
+        {
+            // Arrange
+
+
+
+            // Act
+            var result = TestHelper.ProductService.AddRating(null, 1);
+
+
+
+            // Assert
+            Assert.AreEqual(false, result);
+        }
+
+
+
+        /// <summary>
+        ///  Testing OnGet Valid should return all products
+        /// </summary>
         [Test]
         public void OnGet_Valid_Should_Return_Products()
         {
-            // Arrange
-            var data = TestHelper.ProductService.GetAllData().First();
+            //Arrange
+
+
 
             // Act
-            pageModel.OnGet(data.Id);
+            pageModel.OnGet("jenlooper-cactus");
+
+
 
             // Assert
             Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            
         }
-        #endregion OnGet
+
+
+
+        /// <summary>
+        ///  Testing OnGet invalid should return false
+        /// </summary>
+        [Test]
+        public void OnGet_InValid_Should_Return_False()
+        {
+            // Arrange
+
+
+
+            // Act
+            pageModel.OnGet("ABC");
+
+
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+           
+        }
+
+
+
+        /// <summary>
+        /// Testing GetAllData for Valid Product and valid rating should return true
+        /// </summary>
+        [Test]
+        public void GetAllData_Valid_Product_Valid_Rating_Valid_Should_Return_True()
+        {
+            // Arrange
+
+
+
+            // Get the First data item
+            var data = TestHelper.ProductService.GetAllData().First();
+            var countOriginal = data.Ratings.Length;
+
+
+
+            // Act
+            var result = TestHelper.ProductService.AddRating(data.Id, 5);
+            var dataNewList = TestHelper.ProductService.GetAllData().First();
+
+
+
+            // Assert
+            Assert.AreEqual(true, result);
+            Assert.AreEqual(countOriginal + 1, dataNewList.Ratings.Length);
+            Assert.AreEqual(5, dataNewList.Ratings.Last());
+        }
+
+
+
+        #endregion
     }
 }
