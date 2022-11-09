5d509cc830290405edd0450b87d08086ea950407
Pranindhar3123 <pranindhar111@gmail.com>
2022-11-08 18:35:13 -0800

    Added Comments in Unit Tests for Models <ProductModelTests.cs>

--- UnitTests/Models/ProductModelTests.cs
+++ UnitTests/Models/ProductModelTests.cs
@@ -8,12 +8,12 @@ using NUnit.Framework;
 namespace UnitTests.Models
 {
     /// <summary>
-    /// Test class for ArticleModel
+    /// Test class for ProductModel
     /// </summary>
     public class ProductModelTests
     {
         /// <summary>
-        /// Method to test valid input of an article
+        /// Method to test valid input of an product
         /// </summary>
         [Test]
         public void ToString_Valid_Null_ID_Should_Return_True()
