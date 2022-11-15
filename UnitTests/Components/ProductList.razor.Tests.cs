a1759d29228da728deba1474b8b6b5e94c05594f
Pranindhar3123 <pranindhar111@gmail.com>
2022-11-15 15:35:37 -0800

    Added comments for the razor <ProductList.razor.Tests.cs>

--- UnitTests/Components/ProductList.razor.Tests.cs
+++ UnitTests/Components/ProductList.razor.Tests.cs
@@ -1,15 +1,23 @@
 namespace UnitTests.Components
 {
+    using System.Linq;
+    using Bunit;
     using ContosoCrafts.WebSite.Components;
     using ContosoCrafts.WebSite.Services;
 
+
+
     using Microsoft.Extensions.DependencyInjection;
+
+
 
     using NUnit.Framework;
 
 
+
+
     /// <summary>
-    /// Article list test set.
+    /// Product list test set.
     /// </summary>
     public class ProductListTests : BunitTestContext
     {
@@ -20,11 +28,14 @@ namespace UnitTests.Components
         [SetUp]
         public void TestInitialize()
         {
+
         }
+
+
 
         #endregion TestSetup
         /// <summary>
-        /// Test for returning list of articles. 
+        /// Test for returning list of products.
         /// </summary>
         [Test]
         public void ProductList_Default_Should_Return_Content()
@@ -32,14 +43,55 @@ namespace UnitTests.Components
             // Arrange
             _ = Services.AddSingleton<JsonFileProductService>(TestHelper.ProductService);
 
+
+
             // Act
             var page = RenderComponent<ProductList>();
+
+
 
             // Get the Cards retrned
             var result = page.Markup;
 
+
+
             // Assert
             Assert.AreEqual(true, result.Contains("Seattle Public School"));
+        }
+
+
+
+        /// <summary>
+        /// Unit test to validate Select Product click
+        /// </summary>
+        [Test]
+        public void SelectProduct_Valid_ID_jenlooper_Should_Return_Content()
+        {
+            // Arrange
+            Services.AddSingleton<JsonFileProductService>(TestHelper.ProductService);
+            var id = "MoreInfoButton_jenlooper-cactus";
+            var page = RenderComponent<ProductList>();
+
+
+
+            // Find More Info button specific to id
+            var buttonList = page.FindAll("Button");
+            var button = buttonList.First(m => m.OuterHtml.Contains(id));
+
+
+
+            // Act
+            button.Click();
+
+
+
+            // Store markup for Assert statement
+            var pageMarkup = page.Markup;
+
+
+
+            // Assert
+            Assert.AreEqual(true, pageMarkup.Contains("2445 3rd Ave. S, Seattle, WA 98134"));
         }
     }
 }
