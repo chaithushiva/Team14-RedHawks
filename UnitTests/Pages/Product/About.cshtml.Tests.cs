2884bfca2ee1579927271079c96053524c857328
chaithushiva <sgoli@seattleu.edu>
2022-11-20 17:32:24 -0800

    Added Unit Tests for About page <About.cshtml.Tests.cs>

---
+++ UnitTests/Pages/Product/About.cshtml.Tests.cs
@@ -0,0 +1,56 @@
+﻿namespace UnitTests.Pages
+{
+    using ContosoCrafts.WebSite.Pages;
+    using Microsoft.Extensions.Logging;
+    using Moq;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Test class for the About page
+    /// </summary>
+    public class AboutTests
+    {
+        #region TestSetup
+        // page model for class
+        public static AboutModel pageModel;
+
+        /// <summary>
+        /// Setup the test prior to execution
+        /// </summary>
+        [SetUp]
+
+        public void TestInitialize()
+        {
+            var MockLoggerDirect = Mock.Of<ILogger<AboutModel>>();
+
+            pageModel = new AboutModel(MockLoggerDirect)
+            {
+                PageContext = TestHelper.PageContext,
+                TempData = TestHelper.TempData,
+            };
+        }
+
+        #endregion TestSetup
+
+        #region OnGet
+
+        /// <summary>
+        /// Test OnGet method for valid result
+        /// </summary>
+        [Test]
+        public void OnGet_Valid_Activity_Set_Should_Return_RequestId()
+        {
+            // Arrange
+
+            // Act
+            pageModel.OnGet();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+        }
+
+        #endregion OnGet
+    }
+}
