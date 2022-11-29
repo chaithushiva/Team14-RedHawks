1b2721b0346cb7fd0872672a48ca45c8db915d7c
chaithushiva <sgoli@seattleu.edu>
2022-11-29 14:38:55 -0800

    Added UnitTests to <ProductControllerTests.cs>

--- UnitTests/Controllers/ProductsControllerTests.cs
+++ UnitTests/Controllers/ProductsControllerTests.cs
@@ -1,12 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace UnitTests.Controllers
+﻿namespace UnitTests.Controllers
 {
+    using System.Linq;
+    using ContosoCrafts.WebSite.Controllers;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Unit tests for the ArticleController class.
+    /// </summary>
     internal class ProductControllerTests
     {
+        #region TestSetup
+
+        // controller instance to test
+        public static ProductsController controller;
+
+        /// <summary>
+        /// SetUp for the tests
+        /// </summary>
+        [SetUp]
+        public void TestInitialize()
+        {
+            // Create a new controller instance
+            controller = new ProductsController(TestHelper.ProductService);
+        }
+
+        #endregion TestSetup
+
+        #region Get
+
+        /// <summary>
+        /// Test the API Get Http request method
+        /// </summary>
+        [Test]
+        public void Get_Valid_Should_Returns_All_Product()
+        {
+            // Arrange
+            var expected = TestHelper.ProductService.GetAllData().Count();
+
+            // Act
+            var actual = controller.Get().Count();
+
+            // Assert
+            Assert.AreEqual(true, controller.ModelState.IsValid);
+            Assert.AreEqual(expected, actual);
+        }
+
+        #endregion Get
     }
 }
