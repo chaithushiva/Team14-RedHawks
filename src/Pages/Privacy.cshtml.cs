5f6547ce43145484a045dde380945789987ee3fc
chaithushiva <sgoli@seattleu.edu>
2022-11-29 14:32:06 -0800

    Added Comments in <Privacy.cshtml.cs>

--- src/Pages/Privacy.cshtml.cs
+++ src/Pages/Privacy.cshtml.cs
@@ -1,17 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
-
-namespace ContosoCrafts.WebSite.Pages
+﻿namespace ContosoCrafts.WebSite.Pages
 {
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+    using Microsoft.Extensions.Logging;
+
+    /// <summary>
+    /// Privacy page.
+    /// </summary>
     public class PrivacyModel : PageModel
     {
         private readonly ILogger<PrivacyModel> _logger;
 
+        /// <summary>
+        /// Default constructor for the Privacy page.
+        /// </summary>
+        /// <param name="logger">Takes a ILogger.</param>
         public PrivacyModel(ILogger<PrivacyModel> logger)
         {
             _logger = logger;
         }
 
+        /// <summary>
+        /// REST Get request
+        /// </summary> 
         public void OnGet()
         {
         }
